3daf5a478752615068ee283e54926a6a221d80c0
Emil Skindersø <sprint702emil@gmail.com>
2024-03-15 12:21:30 +0100

    nyeste shap træner

--- scripts/SHAP ranger.R
+++ scripts/SHAP ranger.R
@@ -14,13 +14,14 @@ task = add_weight(task,weighting="interest")
 
 random_forest$train(task)
 saveRDS(random_forest,file="modeller/random_forest_trained")
+random_forest<- readRDS("modeller/random_forest_trained")
 
 ranger_explain = DALEXtra::explain_mlr3(random_forest,
                                         data=train[,-19],
                                         y=train[,19])
 
 saveRDS(ranger_explain,file="shap/random_forest_explain")
-readRDS("shap/random_forest_explain")
+ranger_explain<- readRDS("shap/random_forest_explain")
 
 my_dataset_with_index <- train %>%
   mutate(original_index = row_number())
@@ -29,14 +30,16 @@ set.seed(1)
 dat_exp1<-sample_n(filter(my_dataset_with_index,ClaimInd==1),1)#Ignore the index
 dat_exp2<-sample_n(filter(my_dataset_with_index,ClaimInd==0),1)
 
-shap_rf1<- plot(predict_parts(ranger_explain,dat_exp1[,-23]))
-shap_rf2<- plot(predict_parts(ranger_explain,dat_exp2[,-23]))
+shap_rf1<- plot(predict_parts(ranger_explain,dat_exp1[,-c(19,23)]))
+shap_rf2<- plot(predict_parts(ranger_explain,dat_exp2[,-c(19,23)]))
 
 predictor <- Predictor$new(ranger_explain,data=train[,-c(3,4,19)],y=train[,19])
 
-future::plan(multisession)
+future::plan(multisession,workers=2)
 
-importance<- FeatureImp$new(predictor,loss="mse",n.repetitions=15)
+importance<- FeatureImp$new(predictor,loss="mse",n.repetitions=10)
+
+#the importance of exposure is probably overestimated due to the weighting
 saveRDS(importance,"random_forest_imp")
 importance$plot()
 
